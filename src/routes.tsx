c4b494da4c305b98ea56ee3893bbc75bfa8efc4a
jeong-hae-rim <usemono33@gmail.com>
2026-04-29 05:21:23 +0900

    feat: 하

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import App from "./pages/App";
-import Secretpage from "./pages/SecretPage";
+// import Secretpage from "./pages/SecretPage";
 import NotFound from "./pages/404";
 // import GamePage from "./pages/GamePage";
-import RollingPage from "./pages/RollingPage";
+// import RollingPage from "./pages/RollingPage";
 import MdPage from "./pages/MdPage";
 
 const Router = () => {
@@ -13,8 +13,8 @@ const Router = () => {
         <Routes>
           {/* <Route path="/" element={<App />} /> */}
           <Route path="/goods" element={<MdPage />} />
-          <Route path="/yummychu" element={<RollingPage />} />
-          <Route path="/:tab?" element={<Secretpage />} />
+          {/* <Route path="/yummychu" element={<RollingPage />} /> */}
+          {/* <Route path="/:tab?" element={<Secretpage />} /> */}
           {/* <Route path="/game2048" element={<GamePage />} /> */}
           <Route path="/*" element={<NotFound />} />
         </Routes>
