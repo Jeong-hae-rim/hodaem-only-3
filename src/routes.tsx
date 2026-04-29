a9a4d5540b1802d080e5fb7f5ccf9dc00ecf796a
jeong-hae-rim <usemono33@gmail.com>
2026-04-29 16:38:07 +0900

    feat: 이미지 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -12,7 +12,7 @@ const Router = () => {
       <BrowserRouter>
         <Routes>
           {/* <Route path="/" element={<App />} /> */}
-          <Route path="/goods" element={<MdPage />} />
+          <Route path="/goodslist" element={<MdPage />} />
           {/* <Route path="/yummychu" element={<RollingPage />} /> */}
           {/* <Route path="/:tab?" element={<Secretpage />} /> */}
           {/* <Route path="/game2048" element={<GamePage />} /> */}
